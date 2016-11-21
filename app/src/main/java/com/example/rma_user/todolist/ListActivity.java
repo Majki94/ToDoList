93c869ce2208aba34622b20bd7a76b115e42f567
Majki94 <master.atwork@yahoo.com>
2016-11-21 13:28:37 +0100

    Svasta nesto!

--- app/src/main/java/com/example/rma_user/todolist/ListActivity.java
+++ app/src/main/java/com/example/rma_user/todolist/ListActivity.java
@@ -5,6 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
+
 public class ListActivity extends AppCompatActivity {
 
     String[] data = new String[]{"green", "blue", "red"};
@@ -16,7 +21,14 @@ public class ListActivity extends AppCompatActivity {
 
         ListView listView = (ListView) findViewById(R.id.listView);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String> (this, R.layout.list_item, data);
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String> (this, R.layout.list_item, data);
+
+        Item item = new Item("item1", true, "21.11.2016", "13:25"):
+        Item item2 = new Item("item2", false, "22.11.2016", "13:26"):
+
+        List<Item> list = new ArrayList<Item>();
+        list.add(item);
+        list.add(item2);
 
         listView.setAdapter(adapter);
     }
