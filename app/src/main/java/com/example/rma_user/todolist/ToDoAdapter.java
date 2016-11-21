6f2ded0e90aa062e16378da625d98d504c71529a
Majki94 <master.atwork@yahoo.com>
2016-11-21 13:24:45 +0100

    Svasta nesto!

--- app/src/main/java/com/example/rma_user/todolist/ToDoAdapter.java
+++ app/src/main/java/com/example/rma_user/todolist/ToDoAdapter.java
@@ -1,8 +1,11 @@
 package com.example.rma_user.todolist;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -35,6 +38,22 @@ public class ToDoAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+        if(convertView == null){
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+            convertView = inflater.inflate(R.layout.list_item2, parent, false);
+        }
+
+        TextView textViewItemName = (TextView) convertView.findViewById(R.id.textViewItemName);
+        CheckBox done = (CheckBox) convertView.findViewById(R.id.checkBoxDone);
+        TextView textViewDateTime = (TextView) convertView.findViewById(R.id.textViewDateAndTime);
+
+        textViewItemName.setText(data.get(position).getItem());
+        done.setChecked(data.get(position).isStatus());
+        textViewDateTime.setText(data.get(position).getDate() + " " + data.get(position).getTime());
+
+
+
+        return convertView;
     }
 }
