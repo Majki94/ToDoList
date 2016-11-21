f1cb1265ffcd48e9fc9d232166f01bffc43580e8
Majki94 <master.atwork@yahoo.com>
2016-11-21 12:52:51 +0100

    Sve ok!

--- app/src/main/java/com/example/rma_user/todolist/ListActivity.java
+++ app/src/main/java/com/example/rma_user/todolist/ListActivity.java
@@ -1,13 +1,23 @@
 package com.example.rma_user.todolist;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListActivity extends AppCompatActivity {
+
+    String[] data = new String[]{"green", "blue", "red"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String> (this, R.layout.list_item, data);
+
+        listView.setAdapter(adapter);
     }
 }
